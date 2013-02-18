1ad77cd30170f39eefbe51a9b0e5a6e71588ec03
Christoph Jaeger <email@christophjaeger.info>
2013-02-18 21:04:01 +0100

    xmonad: send title of focused window to GNOME/MATE/Xfce panel applet
    Signed-off-by: Christoph Jaeger <email@christophjaeger.info>

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -9,6 +9,11 @@ import XMonad.Config.Desktop
 import XMonad.Config.Gnome
 import XMonad.Config.Kde
 import XMonad.Config.Xfce
+import XMonad.Hooks.DynamicLog
+
+import qualified DBus as DBus
+import qualified DBus.Client as C
+import qualified Codec.Binary.UTF8.String as UTF8
 
 myModMask = mod4Mask
 
@@ -19,12 +24,33 @@ myKeys conf@(XConfig {XMonad.modMask = m}) = Map.fromList $
 	, ((m .|. shiftMask, xK_Right), shiftToNext >> nextWS)
 	]
 
+myLogHook dbus = defaultPP
+	{ ppCurrent = const ""
+	, ppVisible = const ""
+	, ppHidden  = const ""
+	, ppUrgent  = const ""
+	, ppLayout  = const ""
+	, ppSep     = ""
+	, ppOutput  = \ s ->
+		C.emit dbus (DBus.signal (DBus.objectPath_ "/org/xmonad/Log")
+		                         (DBus.interfaceName_ "org.xmonad.Log")
+		                         (DBus.memberName_ "Update"))
+			{DBus.signalBody = [DBus.toVariant (UTF8.decodeString s)]}
+	}
+
 main = do
 	session <- getEnv "DESKTOP_SESSION"
 	let config = maybe desktopConfig desktop session
+	dbus <- C.connectSession
+	C.requestName dbus (DBus.busName_ "org.xmonad.Log")
+		[ C.nameAllowReplacement
+		, C.nameReplaceExisting
+		, C.nameDoNotQueue
+		]
 	xmonad $ config
 		{ modMask = myModMask
 		, keys    = myKeys <+> keys config
+		, logHook = dynamicLogWithPP (myLogHook dbus) <+> logHook config
 		}
 
 desktop s = case s of
