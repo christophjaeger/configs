dea749dd00256e3de38fdbebe8e030b6db03ad7e
Christoph Jaeger <email@christophjaeger.info>
2013-02-18 20:43:15 +0100

    xmonad: add key bindings to cycle through workspaces
    Signed-off-by: Christoph Jaeger <email@christophjaeger.info>

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,7 +1,10 @@
 import System.Posix.Env (getEnv)
 import Data.Maybe (maybe)
 
+import qualified Data.Map as Map
+
 import XMonad
+import XMonad.Actions.CycleWS
 import XMonad.Config.Desktop
 import XMonad.Config.Gnome
 import XMonad.Config.Kde
@@ -9,10 +12,20 @@ import XMonad.Config.Xfce
 
 myModMask = mod4Mask
 
+myKeys conf@(XConfig {XMonad.modMask = m}) = Map.fromList $
+	[ ((m              , xK_Left ), prevWS)
+	, ((m              , xK_Right), nextWS)
+	, ((m .|. shiftMask, xK_Left ), shiftToPrev >> prevWS)
+	, ((m .|. shiftMask, xK_Right), shiftToNext >> nextWS)
+	]
+
 main = do
 	session <- getEnv "DESKTOP_SESSION"
-	xmonad $ (maybe desktopConfig desktop session)
-		{ modMask = myModMask }
+	let config = maybe desktopConfig desktop session
+	xmonad $ config
+		{ modMask = myModMask
+		, keys    = myKeys <+> keys config
+		}
 
 desktop s = case s of
 	"gnome"        -> gnomeConfig
